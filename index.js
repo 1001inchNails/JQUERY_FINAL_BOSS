dad0d145a8f3516a8fc3c1b35742bdaf6d6fbffe
1001inchNails <162819331+1001inchNails@users.noreply.github.com>
2024-06-18 14:38:09 +0200

    comentarios

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
     $(document).ready(function() {
         
-    function gatetesFacts() {
+    function gatetesFacts() {    // recogida datos de api, iteracion y adicion de facts
         $.getJSON('https://meowfacts.herokuapp.com/?count=5', function(datos) {
             var factsArray = datos.data;
-            $('#factList').empty();
+            $('#factList').empty();    // vaciamos facts antes de meter nuevos
             factsArray.forEach(fact => {
                 $('#factList').append('<li>' + fact + '</li>');
             });
         });
     }
     gatetesFacts();
-    setInterval(gatetesFacts, 10000);
+    setInterval(gatetesFacts, 10000);    // cambio de facts por nuevos cada 10 segundos
 });
